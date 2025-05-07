31a2f718d790995607c376d2133faaa495537e8a
Leonard <23199153+galustian@users.noreply.github.com>
2025-05-07 10:16:13 -0700

    use uv

--- preprocessing.sh
+++ preprocessing.sh
@@ -5,7 +5,7 @@ FULL_CSV="$DATA_PATH/raw_data/rdb7_full.csv"
 FULL_XYZ="$DATA_PATH/raw_data/rdb7_full.xyz"
 SAVE_DIR="$DATA_PATH/processed_data"
 
-python preprocessing.py \
+uv run preprocessing.py \
     --csv_file "$FULL_CSV" \
     --xyz_file "$FULL_XYZ" \
     --save_dir "$SAVE_DIR"
